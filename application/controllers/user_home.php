78415d3bee6210f6cd8595f8b3b92fcfe9ab5d6b
Tahmid Nishat <tahmid.ni7@gmail.com>
2019-03-23 13:12:52 +0600

    upgrade user panal

--- application/controllers/user_home.php
+++ application/controllers/user_home.php
@@ -25,4 +25,44 @@ class User_home extends CI_Controller {
 		$view['user_view'] = "users/user_index";
 		$this->load->view('layouts/user_home', $view);
 	}
+
+	public function sell_books()
+	{
+		/*=== LOAD DYNAMIC CATAGORY ===*/
+		$this->load->model('admin_model');
+		$view['category'] = $this->admin_model->get_category();
+		/*==============================*/
+
+		$config = [
+			'upload_path'=>'./uploads/image/',
+			'allowed_types'=>'jpg|png',
+			'max_size' => '400,',
+			'overwrite' => FALSE
+			];
+
+		$this->load->library('upload', $config);
+
+		$this->form_validation->set_rules('book_name', 'Book name', 'trim|required|strip_tags[book_name]');
+		$this->form_validation->set_rules('description', 'Description', 'trim|required|min_length[100]|strip_tags[description]');
+		$this->form_validation->set_rules('author', 'Author name', 'trim|required|alpha_numeric_spaces|strip_tags[author]');
+		$this->form_validation->set_rules('publisher', 'Publisher name', 'trim|required|alpha_numeric_spaces|strip_tags[publisher]');
+		$this->form_validation->set_rules('price', 'Price', 'trim|required|alpha_numeric_spaces|strip_tags[price]');
+		$this->form_validation->set_rules('quantity', 'Quantity', 'trim|required|numeric|strip_tags[quantity]');
+		$this->form_validation->set_rules('categoryId', 'Category', 'trim|required');
+		/*$this->form_validation->set_rules('userfile', 'File', 'trim|required');*/
+
+
+		if(($this->form_validation->run() && $this->upload->do_upload()) == FALSE)
+		{
+
+			$view['user_view'] = "users/sell_books";
+			$this->load->view('layouts/user_home', $view);
+
+		}
+		else
+		{
+
+		}
+		
+	}
 }
--- application/views/layouts/user_home.php
+++ application/views/layouts/user_home.php
@@ -91,7 +91,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="container">
             <div class="user-menu">
             <ul>
-                <li><a href="">Sell Books</a></li>
+                <li><a href="<?= base_url()?>user_home/sell_books">Sell Books</a></li>
                 <li><a href="">My books</a></li>
                 <li><a href="">My Account</a></li>
                 <li><a href="<?= base_url()?>users/logout"><i class="fas fa-power-off"></i> Logout</a></li>
