1119818adfc0eb78d9a9d58c2832e40d168925ac
Tahmid Nishat <tahmid.ni7@gmail.com>
2019-04-15 13:39:35 +0600

    Make Onclick drop-down category
    Previously it was with a:hover.
    
    This is bootstrap onclick drop-down with link.

--- application/views/temp/menu.php
+++ application/views/temp/menu.php
@@ -2,20 +2,25 @@
     <div class="container">
         <div class="main-menu">
             <div class="row">
-                <div class="col-md-3">
-                    <div class="onclick-dropdown">
-                        <button id="ctg">Books Category <span id="m-list"><i class="fas fa-list"></i></span></button>
-                     
-                        <ul class="active">
-                            <?php foreach($category as $ctg): ?>
-                                
-                            <?php print '<li><a href="'.base_url('users/all-books').'/?ctg='.$ctg->tag.'">'.$ctg->category.'</a></li>';?>
-                           
-                            <?php endforeach; ?>
+                <div class="col-lg-3 col-md-3 col-sm-4">
+                    <!--=== Onclick Drop-down Menu ===-->
+                    <div class="dropdown show onclick-ctg">
+                      <a class="ctg-btn" href="#" role="button" data-toggle="dropdown" aria-haspopup="true">
+                        Books Category <span id="m-list"><i class="fas fa-list"></i></span>
+                      </a>
+
+                      <div class="dropdown-menu">
+                        <ul>
+                        <?php foreach($category as $ctg): ?>
+                            
+                        <?php print '<li><a href="'.base_url('users/all-books').'/?ctg='.$ctg->tag.'">'.$ctg->category.'</a></li>';?>
+                       
+                        <?php endforeach; ?>
                         </ul>
+                      </div>
                     </div>
                 </div>
-                <div class="col-md-9">
+                <div class="col-lg-9 col-md-9 col-sm-8">
                     <div class="menu">
                         <ul>
                             <li><a href="<?= base_url()?>home">Home</a></li>
